23ee9ee3300b4dc075279c14b46bb3383d48194c
xtitaniumx <artemspasenko@gmail.com>
2023-03-03 21:28:22 +0700

    add use cases to di

--- app/src/main/java/com/example/donate/di/DomainModule.kt
+++ app/src/main/java/com/example/donate/di/DomainModule.kt
@@ -1,7 +1,25 @@
 package com.example.donate.di
 
+import com.example.donate.domain.usecase.AddFamilyMemberUseCase
+import com.example.donate.domain.usecase.AuthByEmailUseCase
+import com.example.donate.domain.usecase.CreateFamilyUseCase
+import com.example.donate.domain.usecase.JoinToFamilyUseCase
 import org.koin.dsl.module
 
 val domainModule = module {
+    factory {
+        AddFamilyMemberUseCase()
+    }
 
+    factory {
+        AuthByEmailUseCase(userRepository = get())
+    }
+
+    factory {
+        CreateFamilyUseCase()
+    }
+
+    factory {
+        JoinToFamilyUseCase()
+    }
 }
